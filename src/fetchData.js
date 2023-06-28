28c07dab92cb7d63ea20656dcaec82be2d8a7303
Callum <callumacourtt@gmail.com>
2023-06-28 18:34:38 +0100

    Added error handling to weather fetch function

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,14 @@
 export default async function getWeather(location) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=33254ebb9692453c8e6182945231406&q=${location}`
-  );
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=33254ebb9692453c8e6182945231406&q=${location}`
+    );
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('An error occurred while fetching weather data:', error);
+  }
 }
